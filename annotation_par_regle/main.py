a307f28e2952b4c672f2c8b56691778442671c2b
Plume <dami.conceicao57@gmail.com>
2025-01-06 16:35:08 +0100

    🚀 Write main to annotate + add results

--- annotation_par_regle/main.py
+++ annotation_par_regle/main.py
@@ -8,16 +8,21 @@ from pathlib import Path
 from scripts.clean_data import clean_files #no argument, need folder raw_data and clean_data
 from scripts.clean_annotation import correct_annotation #arguments: syllabed, annotated
 from scripts.syllabification import divide_verse_into_syllables #argument: verse
+from scripts.annotate import annotation #arg
 
 st.nouveau_dossier("clean_data")
+st.nouveau_dossier("annotated_data")
 clean_files()
 
 #print(divide_verse_into_syllables('quoi quel qualité quais laquais est'))
 
-for p in Path('clean_data/').glob('*.txt'):
+for p in Path('./clean_data/').glob('*.txt') :
+	print(p)
+	filename = "./annotated_data/" + str(p)[11:len(str(p))]
 	content = st.file_to_lines(str(p))
-	syllabed = []
+	st.new_empty_file(filename)
 	for line in content:
 		s_verse = divide_verse_into_syllables(line.lower())
-		syllabed.append(s_verse)
-		print(syllabed)		
+		ano_verse = annotation(s_verse)
+		with open(filename, 'a', encoding="utf-8") as f:
+			f.write(ano_verse + '\n')
